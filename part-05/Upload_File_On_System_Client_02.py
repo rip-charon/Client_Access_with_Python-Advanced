a0335c2d9d246a957b9c85867a27a031e2c56c9f
CHARON <rip.charon@gmail.com>
2021-08-25 20:08:04 +0430

    done part-5

--- part-05/Upload_File_On_System_Client_02.py
+++ part-05/Upload_File_On_System_Client_02.py
@@ -3,20 +3,16 @@ from socket import *
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(("192.168.1.115", 5050))
 
-s.send("Hello server!".encode('utf-8'))
+s.send("Hello server! Send me a file !".encode('utf-8'))
 
-with open('received_file', 'wb') as f:
-    print('file opened')
+with open('mynewtext.txt', 'wb') as f:
+    
     while True:
-        print('receiving data...')
         data = s.recv(1024)
-        print('data=%s', (data))
         if not data:
             break
         # write data to a file
         f.write(data)
 
 f.close()
-print('Successfully get the file')
 s.close()
-print('connection closed')
